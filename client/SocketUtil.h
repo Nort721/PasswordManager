6fffc6cb2b078fe200b8d5b999a17dc42d642018
Nort721 <itag7894@gmail.com>
2021-12-18 21:20:04 +0200

    removed useless master folder

---
+++ client/SocketUtil.h
@@ -0,0 +1,8 @@
+#ifndef SocketUtil_H    // To make sure you don't declare the function more than once by including the header multiple times.
+#define SocketUtil_H
+
+#include <string>
+
+std::string SendSocketMessage(std::string msg);
+
+#endif
--- passwordmanager/client/SocketUtil.h
+++
@@ -1,8 +0,0 @@
-#ifndef SocketUtil_H    // To make sure you don't declare the function more than once by including the header multiple times.
-#define SocketUtil_H
-
-#include <string>
-
-std::string SendSocketMessage(std::string msg);
-
-#endif
